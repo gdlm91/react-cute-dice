9e16aadf0fb54a6b44a0a9af6a28b40b8c777be4
Gerardo Leal <gerard.leal91@gmail.com>
2020-09-14 15:15:24 +1000

    Fixed storybook addons order

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,6 @@ module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     {
       name: "@storybook/addon-docs",
@@ -10,5 +9,6 @@ module.exports = {
         configureJSX: true,
       },
     },
+    "@storybook/addon-essentials",
   ],
 };
